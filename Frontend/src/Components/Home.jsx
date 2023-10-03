a0306b2091e1848e0568b22208db25e931aca284
Manan Bhatia <mananbhatia02@gmail.com>
2023-10-03 22:51:16 +0530

    Initial setup

---
+++ Frontend/src/Components/Home.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    const name ="manan"
+    return (
+        <>
+        <div>
+            <h1 className="text-blue-800">Home</h1>
+        </div>
+        </>
+    )
+}
