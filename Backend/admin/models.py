a11a457e0972e8a425762f7af76e70b7142c8019
Romi Gupta <romi35532@gmail.com>
2023-10-05 21:50:37 +0530

    Done with Blog-Post Model

--- Backend/admin/models.py
+++ Backend/admin/models.py
@@ -1,1 +1,19 @@
-import sqlalchemy
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db' 
+
+db = SQLAlchemy(app)
+
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    thumbnail = db.Column(db.String(255), nullable=False)
+    image1 = db.Column(db.String(255))
+    image2 = db.Column(db.String(255))
+    description = db.Column(db.Text, nullable=False)
+    created_on = db.Column(db.DateTime, default=datetime.utcnow)
+    is_public = db.Column(db.Boolean, default=True)
