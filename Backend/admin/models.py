e62a2d382e4881640d30ac914b4ab293aae7c1ea
Rishabh <saty665566@gmail.com>
2023-10-05 22:01:00 +0530

    updated

--- Backend/admin/models.py
+++ Backend/admin/models.py
@@ -1,4 +1,10 @@
-import sqlalchemy
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import Datetime
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db' 
+db = SQLAlchemy(app)
+
 
 
 # blog-post ka model 
@@ -19,6 +25,19 @@ import sqlalchemy
  # - is_active
  # - date_joined
  # - date_left_on
+class Member(db.model):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.column(db.String(100),nullable=False)
+    image=db.column(db.String(255),nullable=False)
+    position=db.column(db.String(100),nullable=False)
+    order=db.column(db.Integer,nullable=False)
+    is_public=db.column(db.Boolean,default=False)
+    is_active=db.column(db.Boolean,default=False)
+    date_joined=db.column(db.Datetime,default=Datetime.utcnow)
+    date_left_on=db.column(db.Datetime,default=Datetime.utcnow)
 
-
+if __name__=='__main__':
+    db.create_all()
+    app.run(debug=True,port=8000)
+    
  # Model name BlogPost,Member
