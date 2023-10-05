dcb24bd92b22e25a69cc49dfb70a43ad3d557f1c
Romi Gupta <128274320+MasterMindRomii@users.noreply.github.com>
2023-10-05 21:59:05 +0530

    Update models.py
    "Done with Blog-Post Model "

--- Backend/admin/models.py
+++ Backend/admin/models.py
@@ -1,14 +1,22 @@
-import sqlalchemy
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 
-# blog-post ka model 
- # - Title
- # - Thumbnail
- # - Image 1 (optional)
- # - Image 2 (optional)
- # - Description
- # - Created on
- # - is_public (boolean feild)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db' 
+
+db = SQLAlchemy(app)
+
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    thumbnail = db.Column(db.String(255), nullable=False)
+    image1 = db.Column(db.String(255))
+    image2 = db.Column(db.String(255))
+    description = db.Column(db.Text, nullable=False)
+    created_on = db.Column(db.DateTime, default=datetime.utcnow)
+    is_public = db.Column(db.Boolean, default=True)
 
 # members ka model
  # - Name
