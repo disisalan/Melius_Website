0946ea2b92f07c26c9804836d3707108d132061c
RaviKumar920570 <146867815+RaviKumar920570@users.noreply.github.com>
2023-10-07 13:03:16 +0530

    Update urls.py

--- Backend/admin/urls.py
+++ Backend/admin/urls.py
@@ -32,42 +32,41 @@ def contactus():
 @admin_bp.route('members/', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def members():
     if request.method == 'GET':
-         # Read operation - Fetch members from the database
         members = Member.query.all()
-        # Convert the members to a list of dictionaries for JSON response
-        members_list = [{"id": member.id, "name": member.name, "image": member.image,
-                         "position": member.position, "order": member.order,
-                         "is_public": member.is_public, "is_active": member.is_active,
-                         "date_joined": member.date_joined.isoformat(),
-                         "date_left_on": member.date_left_on.isoformat()} for member in members]
+        members_list = []
+        for member in members:
+            members_list.append({
+                "id": member.id,
+                "name": member.name,
+                "image": member.image,
+                "position": member.position,
+                "order": member.order,
+                "is_public": member.is_public,
+                "is_active": member.is_active,
+                "date_joined": member.date_joined.isoformat(),
+                "date_left_on": member.date_left_on.isoformat() if member.date_left_on else None
+            })
         return jsonify(members_list)
 
     elif request.method == 'PUT':
-        # Update operation - Update an existing member
         member_id = request.json.get("id")
         member = Member.query.get(member_id)
-        # Update the member's attributes based on the request data
         member.name = request.json.get("name")
         member.image = request.json.get("image")
         member.position = request.json.get("position")
         member.order = request.json.get("order")
         member.is_public = request.json.get("is_public")
         member.is_active = request.json.get("is_active")
-        # You can also update date_joined and date_left_on if needed
-
         db.session.commit()
         return jsonify({"message": "Member updated successfully"})
 
     elif request.method == 'POST':
-        # Create operation - Add a new member
         name = request.json.get("name")
         image = request.json.get("image")
         position = request.json.get("position")
         order = request.json.get("order")
         is_public = request.json.get("is_public")
         is_active = request.json.get("is_active")
-
-        # Create a new Member object and add it to the database
         new_member = Member(name=name, image=image, position=position, order=order,
                             is_public=is_public, is_active=is_active)
         db.session.add(new_member)
@@ -75,7 +74,6 @@ def members():
         return jsonify({"message": "Member created successfully"})
 
     elif request.method == 'DELETE':
-        # Delete operation - Delete a member
         member_id = request.args.get("id")
         member = Member.query.get(member_id)
         db.session.delete(member)
