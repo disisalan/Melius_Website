a6e6e44bb3077fbf248ca6e47bf7bd632f0dcdd4
Alan <alansijok@gmail.com>
2023-10-09 20:11:50 +0530

    deploy try 1

--- Frontend/src/Components/Homepage.jsx
+++ Frontend/src/Components/Homepage.jsx
@@ -4,7 +4,7 @@ import Home from "./Home";
 export default function Hompage(){
     return(
     <div className="relative " >
-        <div className="absolute h-[100%] w-[100%] bg-[url('/Media/pexels-charlotte-may-5825604.jpg')] bg-cover rotate-180 -z-10"></div>
+        <div className="absolute h-[100%] w-[100%] bg-[url('../Media/pexels-charlotte-may-5825604.jpg')] bg-cover rotate-180 -z-10"></div>
         <Navbar/>
         <Home/>
     </div>
