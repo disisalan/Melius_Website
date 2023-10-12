f614ce56743acce65906cddddbb9dc4f7a70084d
Manan Bhatia <mananbhatia02@gmail.com>
2023-10-12 22:23:22 +0530

    event page added

---
+++ Frontend/src/Components/Events.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Events() {
+    return (
+        <div>
+            <div>
+                <span>
+                    <img src="" alt="" />
+                    <p></p>
+                    <img src="" alt="" />
+                </span>
+            </div>
+        </div>
+    );
+}
+
+export default Events;
