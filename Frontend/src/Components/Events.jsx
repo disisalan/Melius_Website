f4abc033f32e4aee7a75c450c5ac7117506dc1f1
eaSc01 <ishant9715@gmail.com>
2023-10-13 09:55:05 +0530

    Events page added

--- Frontend/src/Components/Events.jsx
+++ Frontend/src/Components/Events.jsx
@@ -2,7 +2,77 @@ import React from "react";
 
 function Events() {
     return (
-        <div>Events</div>
+        <div className="bg-black font-inter px-6 lg:px-24 text-white">
+            <h1 className="py-5 font-poppins text-3xl lg:text-5xl font-bold border-b-4 border-blue">Events</h1>  
+
+            {/* Events Container */}
+            <div className=" py-5 md:py-10 overflow-x-auto flex flex-nowrap gap-5 md:gap-10">
+                
+                {/* Event */}
+                <div className="relative bg-[url('../Media/bg-2.png')] bg-contain w-[200px] h-[266px] md:w-[400px] md:h-[533px] flex-none">
+                    {/* Event Text */}
+                    <div className="absolute left-2 md:left-4 bottom-2 font-poppins font-bold text-3xl md:text-5xl">
+                        <h1>Event</h1>
+                        <h1 className="text-blue">Shiksha</h1>
+                    </div>
+                </div>
+                <div className="relative bg-[url('../Media/bg-2.png')] bg-contain w-[200px] h-[266px] md:w-[400px] md:h-[533px] flex-none">
+                    {/* Event Text */}
+                    <div className="absolute left-2 md:left-4 bottom-2 font-poppins font-bold text-3xl md:text-5xl">
+                        <h1>Event</h1>
+                        <h1 className="text-blue">Shiksha</h1>
+                    </div>
+                </div>
+                <div className="relative bg-[url('../Media/bg-2.png')] bg-contain w-[200px] h-[266px] md:w-[400px] md:h-[533px] flex-none">
+                    {/* Event Text */}
+                    <div className="absolute left-2 md:left-4 bottom-2 font-poppins font-bold text-3xl md:text-5xl">
+                        <h1>Event</h1>
+                        <h1 className="text-blue">Shiksha</h1>
+                    </div>
+                </div>
+                <div className="relative bg-[url('../Media/bg-2.png')] bg-contain w-[200px] h-[266px] md:w-[400px] md:h-[533px] flex-none">
+                    {/* Event Text */}
+                    <div className="absolute left-2 md:left-4 bottom-2 font-poppins font-bold text-3xl md:text-5xl">
+                        <h1>Event</h1>
+                        <h1 className="text-blue">Shiksha</h1>
+                    </div>
+                </div>
+                <div className="relative bg-[url('../Media/bg-2.png')] bg-contain w-[200px] h-[266px] md:w-[400px] md:h-[533px] flex-none">
+                    {/* Event Text */}
+                    <div className="absolute left-2 md:left-4 bottom-2 font-poppins font-bold text-3xl md:text-5xl">
+                        <h1>Event</h1>
+                        <h1 className="text-blue">Shiksha</h1>
+                    </div>
+                </div>
+                <div className="relative bg-[url('../Media/bg-2.png')] bg-contain w-[200px] h-[266px] md:w-[400px] md:h-[533px] flex-none">
+                    {/* Event Text */}
+                    <div className="absolute left-2 md:left-4 bottom-2 font-poppins font-bold text-3xl md:text-5xl">
+                        <h1>Event</h1>
+                        <h1 className="text-blue">Shiksha</h1>
+                    </div>
+                </div>
+                <div className="relative bg-[url('../Media/bg-2.png')] bg-contain w-[200px] h-[266px] md:w-[400px] md:h-[533px] flex-none">
+                    {/* Event Text */}
+                    <div className="absolute left-2 md:left-4 bottom-2 font-poppins font-bold text-3xl md:text-5xl">
+                        <h1>Event</h1>
+                        <h1 className="text-blue">Shiksha</h1>
+                    </div>
+                </div>
+
+            </div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem dolores velit, accusantium esse iusto assumenda aut mollitia explicabo! Reprehenderit accusamus fugit aperiam officia facilis impedit obcaecati laborum placeat illo labore.
+            Magnam amet atque nobis dicta adipisci facere, officia, officiis fuga sed rerum nulla maiores consectetur eos corrupti minus omnis? Esse, ipsa! Hic molestias, commodi consequatur repudiandae eum necessitatibus facere debitis?
+            Deleniti saepe dolor maxime deserunt iusto, sed optio unde facilis, reiciendis assumenda vitae tenetur minus nostrum vel iure qui expedita! Inventore optio corrupti temporibus! Maiores fugiat vero eligendi nostrum repellendus!
+            Assumenda rerum amet harum id labore ipsum odit! Consequuntur quos ipsum cum facere eum placeat ea, eaque doloremque quaerat repellendus, voluptas molestiae voluptatibus dolore aperiam eveniet temporibus suscipit laboriosam numquam.
+            Voluptas facere sunt doloremque fugiat atque commodi veritatis assumenda optio, incidunt tempora excepturi blanditiis architecto dolore quia sapiente, natus cum maxime impedit, odio provident voluptatum. Perferendis velit inventore recusandae quaerat!
+            Temporibus, facere. Aperiam ut reprehenderit magni culpa omnis ullam ex eligendi inventore quisquam nobis nihil quos harum libero, repudiandae earum ab magnam iure pariatur, veritatis, autem voluptatem temporibus? Voluptate, architecto!
+            Facere ipsum consequuntur eveniet eum explicabo quas fuga. Eaque obcaecati tempore ab, quos omnis ad architecto error ipsum nihil est fugiat natus voluptas corrupti iusto facere labore quibusdam eum libero.
+            Accusamus necessitatibus quasi porro neque aspernatur iste harum in, quis, earum excepturi doloremque, totam recusandae fugit soluta! Suscipit, quisquam nulla ipsum laborum corporis maxime quod dolor. Quos quam sed amet?
+            Nihil sunt numquam molestias ullam esse ipsa corporis ut a praesentium non reiciendis perferendis asperiores autem, assumenda incidunt facere rerum consequuntur nulla veritatis laboriosam. Vel, laborum! Iusto a atque eveniet.
+            Cumque fugit maxime quae vel minus accusamus reiciendis! Atque qui similique, exercitationem incidunt adipisci aliquam rerum nobis quidem dicta alias officia dolor quibusdam delectus maiores consequuntur earum culpa modi cupiditate.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem laboriosam vero quam ducimus quia eaque culpa illo ut laudantium, maxime non iusto officiis, delectus nemo animi cum sint nesciunt vitae?
+        </div>
+        
     );
 }
 
