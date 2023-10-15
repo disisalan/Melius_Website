ddfc432c916476ab86df49e35d66eabb17735c62
Alan <alansijok@gmail.com>
2023-10-15 23:31:41 +0530

    Event detail Page

--- Frontend/src/Components/event_detail.jsx
+++ Frontend/src/Components/event_detail.jsx
@@ -5,9 +5,10 @@ export default function EventDetail(){
     return(
     <div className="bg-black font-inter px-6  lg:px-24 text-white">
         <div className="p-5">
-        <h1 className="py-5  text-blue text-3xl font-poppins lg:text-5xl font-bold"> Event Heading</h1>
-        <div className="lg:flex gap-8 font-inter">
-            <img src="../Media/bg-1.png" alt="" className="rounded-3xl"/>
+        <h1 className="py-5  text-White text-3xl font-poppins border-b-2 border-blue lg:text-5xl font-bold"> Event Heading</h1>
+        <div className=" py-5 lg:flex gap-8 font-inter">
+            <img src="../Media/bg-1.png" alt="" className="w-[200px] lg:w-[300px] aspect-[10/16] rounded-3xl"/>
+            
             <div className="text-lg">
             <div className=" mb-10 lg:mb-16    ">
                 <h2 className="text-2xl font-poppins font-semibold my-4 text-blue">About the Event </h2>
@@ -48,10 +49,17 @@ export default function EventDetail(){
               <path d="M41,4H9C6.24,4,4,6.24,4,9v32c0,2.76,2.24,5,5,5h32c2.76,0,5-2.24,5-5V9C46,6.24,43.76,4,41,4z M17,20v19h-6V20H17z M11,14.47c0-1.4,1.2-2.47,3-2.47s2.93,1.07,3,2.47c0,1.4-1.12,2.53-3,2.53C12.2,17,11,15.87,11,14.47z M39,39h-6c0,0,0-9.26,0-10 c0-2-1-4-3.5-4.04h-0.08C27,24.96,26,27.02,26,29c0,0.91,0,10,0,10h-6V20h6v2.56c0,0,1.93-2.56,5.81-2.56 c3.97,0,7.19,2.73,7.19,8.26V39z"></path>
             </svg>
           </div>
+          <div  className="flex items-center gap-3 mb-3">
+                <p className="text-xl font-poppins font-semibold text-blue">View Pictures :</p> <span>Click to View Photos</span>
+            </div>
             <div className="w-full flex justify-center">
                 <button className="bg-blue py-4 px-12 rounded-2xl">JOIN NOW</button>
             </div>
         </div>
+        </div>
+        <div>
+        <h1 className="py-5  text-White text-3xl font-poppins lg:text-4xl font-bold">Project Report</h1>
+        <iframe src="" width="100%" height="480" allow="autoplay"></iframe>
         </div>
         </div>
     </div>
