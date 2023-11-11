b6f1bb42af7f3a4734bc4f2359843dec18a6e56e
Manan Bhatia <mananbhatia02@gmail.com>
2023-11-12 00:12:54 +0530

    changes

--- Frontend/src/Components/ProjectDetail.jsx
+++ Frontend/src/Components/ProjectDetail.jsx
@@ -9,16 +9,15 @@ export default function ProjectDetail() {
         "https://meliuswebsite.pythonanywhere.com/api/project_pictures";
     const [project, setproject] = useState([]);
     useEffect(() => {
-        api.get("/projects/?=`${id}`").then((res) => {
+        api.get(`/project/${id}`).then((res) => {
             console.log(res.data);
             setproject(res.data);
         });
     }, []);
     return (
         <div className="bg-black font-inter px-6  lg:px-24 text-white">
-                {project.map((project, index) => (
-                <div className="p-5">
-                <h1 key={index} className="py-5  text-White text-3xl pt-24 font-poppins border-b-2 border-blue lg:text-5xl font-bold">
+            <div className="p-5">
+                <h1 className="py-5  text-White text-3xl pt-24 font-poppins border-b-2 border-blue lg:text-5xl font-bold">
                     {project.name}
                 </h1>
                 <div className=" py-5 lg:flex gap-8 font-inter">
@@ -33,14 +32,13 @@ export default function ProjectDetail() {
                             <h2 className="text-2xl font-poppins font-semibold my-4 text-blue">
                                 About the project{" "}
                             </h2>
-                            <p key={index}  className="">
-                            {project.brief}
-
+                            <p className="">
+                                {project.brief}
                             </p>
                         </div>
                         <div className="flex items-center gap-3">
                             <p className="text-xl font-poppins font-semibold text-blue ">
-                               Partner
+                                Partner
                             </p>
                             <span>{project.partner}</span>
                         </div>
@@ -56,7 +54,7 @@ export default function ProjectDetail() {
                             </p>
                             <span>{project.mode}</span>
                         </div>
-                        
+
                         <div
                             id="social-mob"
                             className="py-8 gap-2 flex items-center [&>svg]:w-9 [&>svg]:h-9  fill-white "
@@ -65,45 +63,46 @@ export default function ProjectDetail() {
                                 Socials :
                             </p>
                             <a target="/" href={project.instagram}>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                x="0px"
-                                y="0px"
-                                height="48"
-                                viewBox="0 0 24 24"
-                            >
-                                <path d="M 8 3 C 5.239 3 3 5.239 3 8 L 3 16 C 3 18.761 5.239 21 8 21 L 16 21 C 18.761 21 21 18.761 21 16 L 21 8 C 21 5.239 18.761 3 16 3 L 8 3 z M 18 5 C 18.552 5 19 5.448 19 6 C 19 6.552 18.552 7 18 7 C 17.448 7 17 6.552 17 6 C 17 5.448 17.448 5 18 5 z M 12 7 C 14.761 7 17 9.239 17 12 C 17 14.761 14.761 17 12 17 C 9.239 17 7 14.761 7 12 C 7 9.239 9.239 7 12 7 z M 12 9 A 3 3 0 0 0 9 12 A 3 3 0 0 0 12 15 A 3 3 0 0 0 15 12 A 3 3 0 0 0 12 9 z"></path>
-                            </svg></a>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    x="0px"
+                                    y="0px"
+                                    height="48"
+                                    viewBox="0 0 24 24"
+                                >
+                                    <path d="M 8 3 C 5.239 3 3 5.239 3 8 L 3 16 C 3 18.761 5.239 21 8 21 L 16 21 C 18.761 21 21 18.761 21 16 L 21 8 C 21 5.239 18.761 3 16 3 L 8 3 z M 18 5 C 18.552 5 19 5.448 19 6 C 19 6.552 18.552 7 18 7 C 17.448 7 17 6.552 17 6 C 17 5.448 17.448 5 18 5 z M 12 7 C 14.761 7 17 9.239 17 12 C 17 14.761 14.761 17 12 17 C 9.239 17 7 14.761 7 12 C 7 9.239 9.239 7 12 7 z M 12 9 A 3 3 0 0 0 9 12 A 3 3 0 0 0 12 15 A 3 3 0 0 0 15 12 A 3 3 0 0 0 12 9 z"></path>
+                                </svg>
+                            </a>
                             <a target="/" href={project.linkedin}>
-
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                x="0px"
-                                y="0px"
-                                height="48"
-                                viewBox="0 0 50 50"
-                            >
-                                <path d="M41,4H9C6.24,4,4,6.24,4,9v32c0,2.76,2.24,5,5,5h32c2.76,0,5-2.24,5-5V9C46,6.24,43.76,4,41,4z M17,20v19h-6V20H17z M11,14.47c0-1.4,1.2-2.47,3-2.47s2.93,1.07,3,2.47c0,1.4-1.12,2.53-3,2.53C12.2,17,11,15.87,11,14.47z M39,39h-6c0,0,0-9.26,0-10 c0-2-1-4-3.5-4.04h-0.08C27,24.96,26,27.02,26,29c0,0.91,0,10,0,10h-6V20h6v2.56c0,0,1.93-2.56,5.81-2.56 c3.97,0,7.19,2.73,7.19,8.26V39z"></path>
-                            </svg></a>
-                        </div>          
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    x="0px"
+                                    y="0px"
+                                    height="48"
+                                    viewBox="0 0 50 50"
+                                >
+                                    <path d="M41,4H9C6.24,4,4,6.24,4,9v32c0,2.76,2.24,5,5,5h32c2.76,0,5-2.24,5-5V9C46,6.24,43.76,4,41,4z M17,20v19h-6V20H17z M11,14.47c0-1.4,1.2-2.47,3-2.47s2.93,1.07,3,2.47c0,1.4-1.12,2.53-3,2.53C12.2,17,11,15.87,11,14.47z M39,39h-6c0,0,0-9.26,0-10 c0-2-1-4-3.5-4.04h-0.08C27,24.96,26,27.02,26,29c0,0.91,0,10,0,10h-6V20h6v2.56c0,0,1.93-2.56,5.81-2.56 c3.97,0,7.19,2.73,7.19,8.26V39z"></path>
+                                </svg>
+                            </a>
+                        </div>
                     </div>
                 </div>
-                { (project.pdf_link)?<div>
-                    <h1 className="py-5  text-White text-3xl font-poppins lg:text-4xl font-bold">
-                        Project Report
-                    </h1>
-                    <iframe
-                        src={project.pdf_link}
-                        width="100%"
-                        height="480"
-                        allow="autoplay"
-                    ></iframe>
-                </div>: <div></div>}
-                
+                {project.pdf_link ? (
+                    <div>
+                        <h1 className="py-5  text-White text-3xl font-poppins lg:text-4xl font-bold">
+                            Project Report
+                        </h1>
+                        <iframe
+                            src={project.pdf_link}
+                            width="100%"
+                            height="480"
+                            allow="autoplay"
+                        ></iframe>
+                    </div>
+                ) : (
+                    <div></div>
+                )}
             </div>
-                ))}
         </div>
     );
 }
-
-
--- Frontend/src/Components/event_detail.jsx
+++ Frontend/src/Components/event_detail.jsx
@@ -2,7 +2,6 @@ import { React, useEffect, useState } from "react";
 import api from "../axios";
 import { useParams } from "react-router-dom";
 
-
 export default function EventDetail() {
     const { id } = useParams();
     console.log(id);
@@ -10,16 +9,15 @@ export default function EventDetail() {
         "https://meliuswebsite.pythonanywhere.com/api/blog_pictures";
     const [event, setEvent] = useState([]);
     useEffect(() => {
-        api.get("/blog_posts/?=`${id}`").then((res) => {
+        api.get(`/blog_posts/${id}`).then((res) => {
             console.log(res.data);
             setEvent(res.data);
         });
     }, []);
     return (
         <div className="bg-black font-inter px-6  lg:px-24 text-white">
-                {event.map((event, index) => (
-                <div className="p-5">
-                <h1 key={index} className="py-5  text-White text-3xl pt-24 font-poppins border-b-2 border-blue lg:text-5xl font-bold">
+            <div className="p-5">
+                <h1 className="py-5  text-White text-3xl pt-24 font-poppins border-b-2 border-blue lg:text-5xl font-bold">
                     {event.title}
                 </h1>
                 <div className=" py-5 lg:flex gap-8 font-inter">
@@ -34,10 +32,7 @@ export default function EventDetail() {
                             <h2 className="text-2xl font-poppins font-semibold my-4 text-blue">
                                 About the Event{" "}
                             </h2>
-                            <p key={index}  className="">
-                            {event.description}
-
-                            </p>
+                            <p className="">{event.description}</p>
                         </div>
                         <div className="flex items-center gap-3">
                             <p className="text-xl font-poppins font-semibold text-blue ">
@@ -57,7 +52,7 @@ export default function EventDetail() {
                             </p>
                             <span>{event.mode}</span>
                         </div>
-                        
+
                         <div
                             id="social-mob"
                             className="py-8 gap-2 flex items-center [&>svg]:w-9 [&>svg]:h-9  fill-white "
@@ -66,55 +61,61 @@ export default function EventDetail() {
                                 Socials :
                             </p>
                             <a target="/" href={event.instagram}>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                x="0px"
-                                y="0px"
-                                height="48"
-                                viewBox="0 0 24 24"
-                            >
-                                <path d="M 8 3 C 5.239 3 3 5.239 3 8 L 3 16 C 3 18.761 5.239 21 8 21 L 16 21 C 18.761 21 21 18.761 21 16 L 21 8 C 21 5.239 18.761 3 16 3 L 8 3 z M 18 5 C 18.552 5 19 5.448 19 6 C 19 6.552 18.552 7 18 7 C 17.448 7 17 6.552 17 6 C 17 5.448 17.448 5 18 5 z M 12 7 C 14.761 7 17 9.239 17 12 C 17 14.761 14.761 17 12 17 C 9.239 17 7 14.761 7 12 C 7 9.239 9.239 7 12 7 z M 12 9 A 3 3 0 0 0 9 12 A 3 3 0 0 0 12 15 A 3 3 0 0 0 15 12 A 3 3 0 0 0 12 9 z"></path>
-                            </svg></a>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    x="0px"
+                                    y="0px"
+                                    height="48"
+                                    viewBox="0 0 24 24"
+                                >
+                                    <path d="M 8 3 C 5.239 3 3 5.239 3 8 L 3 16 C 3 18.761 5.239 21 8 21 L 16 21 C 18.761 21 21 18.761 21 16 L 21 8 C 21 5.239 18.761 3 16 3 L 8 3 z M 18 5 C 18.552 5 19 5.448 19 6 C 19 6.552 18.552 7 18 7 C 17.448 7 17 6.552 17 6 C 17 5.448 17.448 5 18 5 z M 12 7 C 14.761 7 17 9.239 17 12 C 17 14.761 14.761 17 12 17 C 9.239 17 7 14.761 7 12 C 7 9.239 9.239 7 12 7 z M 12 9 A 3 3 0 0 0 9 12 A 3 3 0 0 0 12 15 A 3 3 0 0 0 15 12 A 3 3 0 0 0 12 9 z"></path>
+                                </svg>
+                            </a>
                             <a target="/" href={event.linkedin}>
-
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                x="0px"
-                                y="0px"
-                                height="48"
-                                viewBox="0 0 50 50"
-                            >
-                                <path d="M41,4H9C6.24,4,4,6.24,4,9v32c0,2.76,2.24,5,5,5h32c2.76,0,5-2.24,5-5V9C46,6.24,43.76,4,41,4z M17,20v19h-6V20H17z M11,14.47c0-1.4,1.2-2.47,3-2.47s2.93,1.07,3,2.47c0,1.4-1.12,2.53-3,2.53C12.2,17,11,15.87,11,14.47z M39,39h-6c0,0,0-9.26,0-10 c0-2-1-4-3.5-4.04h-0.08C27,24.96,26,27.02,26,29c0,0.91,0,10,0,10h-6V20h6v2.56c0,0,1.93-2.56,5.81-2.56 c3.97,0,7.19,2.73,7.19,8.26V39z"></path>
-                            </svg></a>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    x="0px"
+                                    y="0px"
+                                    height="48"
+                                    viewBox="0 0 50 50"
+                                >
+                                    <path d="M41,4H9C6.24,4,4,6.24,4,9v32c0,2.76,2.24,5,5,5h32c2.76,0,5-2.24,5-5V9C46,6.24,43.76,4,41,4z M17,20v19h-6V20H17z M11,14.47c0-1.4,1.2-2.47,3-2.47s2.93,1.07,3,2.47c0,1.4-1.12,2.53-3,2.53C12.2,17,11,15.87,11,14.47z M39,39h-6c0,0,0-9.26,0-10 c0-2-1-4-3.5-4.04h-0.08C27,24.96,26,27.02,26,29c0,0.91,0,10,0,10h-6V20h6v2.56c0,0,1.93-2.56,5.81-2.56 c3.97,0,7.19,2.73,7.19,8.26V39z"></path>
+                                </svg>
+                            </a>
                         </div>
                         <div className="flex items-center gap-3 mb-3">
                             <p className="text-xl font-poppins font-semibold text-blue">
                                 View Pictures :
                             </p>{" "}
-                            <a target="/" href={event.drive_link}><span>Click to View Photos</span></a>
+                            <a target="/" href={event.drive_link}>
+                                <span>Click to View Photos</span>
+                            </a>
                         </div>
                         <div className="w-full flex justify-center">
                             <a target="/" href={event.instagram}>
-                            <button className="bg-blue py-4 px-12 rounded-2xl">
-                                JOIN NOW
-                            </button></a>
+                                <button className="bg-blue py-4 px-12 rounded-2xl">
+                                    JOIN NOW
+                                </button>
+                            </a>
                         </div>
                     </div>
                 </div>
-                { (event.pdf_link)?<div>
-                    <h1 className="py-5  text-White text-3xl font-poppins lg:text-4xl font-bold">
-                        Event Report
-                    </h1>
-                    <iframe
-                        src={event.pdf_link}
-                        width="100%"
-                        height="480"
-                        allow="autoplay"
-                    ></iframe>
-                </div>: <div></div>}
-                
+                {event.pdf_link ? (
+                    <div>
+                        <h1 className="py-5  text-White text-3xl font-poppins lg:text-4xl font-bold">
+                            Event Report
+                        </h1>
+                        <iframe
+                            src={event.pdf_link}
+                            width="100%"
+                            height="480"
+                            allow="autoplay"
+                        ></iframe>
+                    </div>
+                ) : (
+                    <div></div>
+                )}
             </div>
-                ))}
         </div>
     );
 }
